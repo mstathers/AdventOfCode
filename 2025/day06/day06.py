a60637a1e3dcafcafdbd1c102d9a9a616694def8
Michael Stathers <mstathers@outlook.com>
2025-12-05 22:57:15 -0800

    day6, p2

--- 2025/day06/day06.py
+++ 2025/day06/day06.py
@@ -16,7 +16,11 @@ def part1(math_homework: str) -> None:
 
             problems[idx].append(pos)
 
+    homework_total = solve_and_total_all_problems(problems)
+    print(f"Part 1: {homework_total}")
 
+
+def solve_and_total_all_problems(problems: dict[int, list]) -> int:
     homework_total = 0
     for problem in problems.values():
         problem_total = int(problem[0])
@@ -32,12 +36,67 @@ def part1(math_homework: str) -> None:
 
         #print(problem, problem_total)
         homework_total += problem_total
+    return homework_total
 
 
-    print(f"Part 1: {homework_total}")
+
+def part2(math_homework: str) -> None:
+    """
+    Going to stick the whole thing in a grid.
+    Then we're going to go through the grid RIGHT to LEFT. We know we get to a
+    problem boundary if we find an operator character. We just ignore the blank
+    columns.
+    After building a problems dictionary, we will solve it just like in part 1.
+    """
+    grid = {}
+    for y, line in enumerate(math_homework.split('\n')):
+        for x, val in enumerate(line):
+            grid[(x, y)] = val
+
+    problems = {}
+    problem_counter = 0
+
+    max_x = max([x[0] for x in grid])
+    max_y = max([y[1] for y in grid])
+
+    x = max_x
+    while x >=0:
+        number = ''
+        operator = None
+        for y in range(max_y+1):
+            if (x, y) in grid:
+                val = grid[(x, y)]
+
+                if val == '*' or val == '+':
+                    operator = val
+                else:
+                    #print(grid[(x, y)])
+                    number += val
+
+
+        #print(f"== {number} ==")
+        if not problem_counter in problems:
+            problems[problem_counter] = []
+        try:
+            problems[problem_counter].append(int(number))
+        except:
+            # this is for the blank "lines"
+            pass
+
+        if operator:
+            problems[problem_counter].append(operator)
+            problem_counter += 1
+
+
+        x = x - 1
+
+
+    homework_total = solve_and_total_all_problems(problems)
+    print(f"Part 2: {homework_total}")
 
 
 if __name__ == '__main__':
     with open('input.txt', encoding="utf-8") as file:
         puzzle_input = file.read()
     part1(puzzle_input)
+    part2(puzzle_input)
